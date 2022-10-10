96019bb10c6e7fbf0c505e2245af3d9b3cd60732
Gus394 <gustavocoelho394@gmail.com>
2022-10-10 16:29:14 -0300

    feito.

--- TrabalhoMercenario/TrabalhoMercenario.cpp
+++ TrabalhoMercenario/TrabalhoMercenario.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> // Feito por Gustavo Coelho
 #include "fila.h"
 #include "simulador.h"
 
@@ -27,22 +27,23 @@ int main()
 
     //std::cout << Estrutura::getSize(f);*/
 
-    Atendimento::Dias dia;
+    Atendimento::Dias dia; // Parte 1
     Atendimento::Secretaria secretaria;
     Estrutura::Fila<int> f;
     Estrutura::inicializar(f);
     Atendimento::atender_clientes(secretaria, dia, f);
 
-    system("pause"); // Parte 1
+    system("pause");
+    system("CLS");
     
-    secretaria.num_atendentes = 12; // 7 caso só sejam atendidas as pendentes e não cheguem mais pessoas além dessas
-    secretaria.atendimentos_por_minuto = secretaria.num_atendentes;
-
     dia.qtd_dias = 3;
+    secretaria.num_atendentes = Atendimento::descobrir_num_de_atendentes(dia, secretaria); //12; // 7 caso só sejam atendidas as pendentes e não cheguem mais pessoas além dessas
+    secretaria.atendimentos_por_minuto = secretaria.num_atendentes;
 
     Atendimento::atender_clientes(secretaria, dia, f);
 
     system("pause"); // Parte 2 - Aleatoriedade
+    system("CLS");
 
     secretaria.num_atendentes = 2;
     dia.pessoas_por_minuto = rand() % 20 + 20;
@@ -50,5 +51,21 @@ int main()
 
     Atendimento::atender_clientes(secretaria, dia, f);
 
-    system("pause"); // Parte 3 -
+    system("pause"); // Parte 3 - Presumindo que sejam 5 pessoas por minuto
+    system("CLS");
+
+    Atendimento::Dias dia2;
+    Atendimento::Secretaria secretaria2;
+    Estrutura::Fila<int> f2;
+    Estrutura::inicializar(f2);
+    
+    dia2.qtd_dias = 30;
+    dia2.minutos = 24 * 60;
+    dia2.pessoas_por_minuto = 5;
+   
+
+    secretaria2.num_atendentes = Atendimento::descobrir_num_de_atendentes(dia2, secretaria2); //15;
+    secretaria2.atendimentos_por_minuto = secretaria2.num_atendentes;
+    Atendimento::atender_clientes(secretaria2, dia2, f2);
+
 }
--- TrabalhoMercenario/simulador.h
+++ TrabalhoMercenario/simulador.h
@@ -7,16 +7,20 @@
 namespace Atendimento {
 	
 	struct Dias {
-		int qtd_dias = 5;
-		int minutos = 480; // 8 horas
-		int pessoas_por_minuto = 5;
+		float qtd_dias = 5;
+		float minutos = 480; // 8 horas
+		float pessoas_por_minuto = 5;
 	};
 
 	struct Secretaria {
-		int num_atendentes = 1;
-		int atendimentos_por_minuto = 1;
-		int atendimentos_pendentes = 0;
-		int atendimentos_efetuados = 0;
+		float num_atendentes = 1;
+		float minutos_de_trabalho = 480;
+		float atendimentos_por_minuto = 1;
+		float atendimentos_pendentes = 0;
+		float atendimentos_efetuados = 0;
+		float aten_por_atendente = 0;
+		float pendentes_por_atendente = 0;
+		float media_atendente = 0;
 	};
 
 	void atender_clientes(Secretaria& sec, Dias dia, Estrutura::Fila<int>& f) {
@@ -31,12 +35,18 @@ namespace Atendimento {
 					Estrutura::queue(f, 1);
 					sec.atendimentos_pendentes++;
 				}
+			}
+			for (int j = 0; j < sec.minutos_de_trabalho; j++) {
 				for (int k = 0; k < sec.atendimentos_por_minuto; k++) {
 					Estrutura::dequeue(f);
 					sec.atendimentos_efetuados++;
 					sec.atendimentos_pendentes--;
 				}
 			}
+			sec.aten_por_atendente = sec.atendimentos_efetuados / sec.num_atendentes;
+			sec.pendentes_por_atendente = sec.atendimentos_pendentes / sec.num_atendentes;
+			sec.media_atendente = sec.atendimentos_efetuados / sec.aten_por_atendente / sec.num_atendentes * 100 / sec.num_atendentes;
+
 			//sec.atendimentos_pendentes += Estrutura::getSize(f);
 
 			while (f.first != nullptr) { // Pessoas na fila indo embora após terminar o dia
@@ -44,11 +54,20 @@ namespace Atendimento {
 			}
 
 			std::cout << " Fim do dia " << i + 1 << std::endl << " Atendentes: " << sec.num_atendentes << std::endl << " Atendimentos efetuados: " << sec.atendimentos_efetuados
-				<< std::endl << " Atendimentos pendentes: " << sec.atendimentos_pendentes << "\n\n";
+				<< std::endl << " Atendimentos pendentes: " << sec.atendimentos_pendentes << "\n\n" << " Atendimentos por atendente: " << sec.aten_por_atendente <<
+				 std::endl << " Pendentes por atendente: " << sec.pendentes_por_atendente << std::endl << " Media de ocupacao diaria por atendente: " << sec.media_atendente <<
+				 "%" << "\n\n";
 
 			system("pause");
 			system("CLS");
 		}
+	}
+
+	float descobrir_num_de_atendentes(Dias dia, Secretaria sec) {
+		if (dia.minutos > sec.minutos_de_trabalho) {
+			sec.atendimentos_pendentes += (dia.minutos - sec.minutos_de_trabalho) * dia.qtd_dias * dia.pessoas_por_minuto;
+		}
+		return (sec.atendimentos_pendentes + dia.pessoas_por_minuto * sec.minutos_de_trabalho * dia.qtd_dias) / ((sec.minutos_de_trabalho) * dia.qtd_dias);
 	}
 }
 
